b0cb6dad41814a0eaa4453e13b818f64bf3a1bb3
Alejandro Osses <alejandro.osses@sorama.eu>
2023-10-20 23:53:26 +0200

    1/3 octave band filtering taken out of the ISO 532-1 code

--- psychoacoustic_metrics/Loudness_ISO532_1/Loudness_ISO532_1.m
+++ psychoacoustic_metrics/Loudness_ISO532_1/Loudness_ISO532_1.m
@@ -84,18 +84,18 @@ if nargin < 6
     end
 end
 
-if method==0
+switch method
+    case 0
     
     if size(insig,1)~=1 % if the insig is not a [Nx1] array
         insig=insig';   % correct the dimension of the insig
     end
     
-elseif method==1 || method==2
-    
-    if size(insig,2)~=1 % if the insig is not a [Nx1] array
-        insig=insig';   % correct the dimension of the insig
-    end
-    
+    otherwise 
+        % method==1 || method==2
+        if size(insig,2)~=1 % if the insig is not a [Nx1] array
+            insig=insig';   % correct the dimension of the insig
+        end
 end
 
 % Time constants for non-linear temporal decay
@@ -112,243 +112,128 @@ SR_LOUDNESS = 500;
 % Tiny value for adjusting intensity levels for stationary signals
 TINY_VALUE = 1e-12;
 % ref value for stationary signals
-I_REF =4e-10;
+I_REF = 4e-10;
+dBFS = 94; % dBFS = 94 means that pref = 2e-5 or, I_REF=pref^2=4e-10
 % bark vector
 barkAxis=(1:240)/10;
 
-%% ***************************
+% ***************************
 % STEP 1 - Resample to 48 kHz
 % ****************************
 
-if method ~= 0 % if different from stationary (from input 1/3 octave unweighted SPL)
-    
-    audio=insig; % input signal
-    
-    if fs ~= 48000
-        gcd_fs = gcd(48000,fs); % greatest common denominator
-        audio = resample(audio,48000/gcd_fs,fs/gcd_fs);
-        fs = 48000;
-        len = size(audio,1);
-    else
-        len = size(audio,1);
-    end
-
-    %% ******************************************************************
-    % Assign values to global variables according to the selected method
-    % *******************************************************************
-    
-    if method == 1 % stationary from audio signal
+switch method
+    case 0
+        % if method == 0, no need to calculate one-third OB levels.
         SampleRateLevel = 1;
         DecFactorLoudness = 1;
         NumSamplesLevel = 1;
-    elseif method == 2 % time_varying from audio signal
-        SampleRateLevel = SR_LEVEL;
-        SampleRateLoudness = SR_LOUDNESS;
-        DecFactorLevel = fs/SampleRateLevel;
-        DecFactorLoudness = SampleRateLevel/SampleRateLoudness;
-        NumSamplesLevel = ceil(len/DecFactorLevel);
-        NumSamplesLoudness = ceil(NumSamplesLevel/DecFactorLoudness);
-    end
-    
-    %% **************************************************
-    % STEP 2 - Create filter bank and filter the signal
-    % ***************************************************
-    
-    % reference
-    br = [1,2,1;1,0,-1;1,-2,1];
-    ar = [1,-2,1;1,-2,1;1,-2,1];
-    
-    CenterFrequency = zeros(28,1);
-    for i = 1:28
-        CenterFrequency(i) = 10^(((i-1)-16)/10.) * 1000; % calculate centre frequencies
-    end
-    
-    % filter 'a' coefficient offsets TABLES A.1 A.2
-    ad = cat(3,[0,-6.70260e-004,6.59453e-004;...
-        0,-3.75071e-004,3.61926e-004;...
-        0,-3.06523e-004,2.97634e-004],... % 25 Hz
-        [0,-8.47258e-004,8.30131e-004;...
-        0,-4.76448e-004,4.55616e-004;...
-        0,-3.88773e-004,3.74685e-004],... % 31.5 Hz
-        [0,-1.07210e-003,1.04496e-003;...
-        0,-6.06567e-004,5.73553e-004;...
-        0,-4.94004e-004,4.71677e-004],... % 40 Hz
-        [0,-1.35836e-003,1.31535e-003;...
-        0,-7.74327e-004,7.22007e-004;...
-        0,-6.29154e-004,5.93771e-004],... % 50 Hz
-        [0,-1.72380e-003,1.65564e-003;...
-        0,-9.91780e-004,9.08866e-004;...
-        0,-8.03529e-004,7.47455e-004],... % 63 Hz
-        [0,-2.19188e-003,2.08388e-003;...
-        0,-1.27545e-003,1.14406e-003;...
-        0,-1.02976e-003,9.40900e-004],... % 80 Hz
-        [0,-2.79386e-003,2.62274e-003;...
-        0,-1.64828e-003,1.44006e-003;...
-        0,-1.32520e-003,1.18438e-003],... % 100 Hz
-        [0,-3.57182e-003,3.30071e-003;...
-        0,-2.14252e-003,1.81258e-003;...
-        0,-1.71397e-003,1.49082e-003],... % 125 Hz
-        [0,-4.58305e-003,4.15355e-003;...
-        0,-2.80413e-003,2.28135e-003;...
-        0,-2.23006e-003,1.87646e-003],... % 160 Hz
-        [0,-5.90655e-003,5.22622e-003;...
-        0,-3.69947e-003,2.87118e-003;...
-        0,-2.92205e-003,2.36178e-003],... % 200 Hz
-        [0,-7.65243e-003,6.57493e-003;...
-        0,-4.92540e-003,3.61318e-003;...
-        0,-3.86007e-003,2.97240e-003],... % 250 Hz
-        [0,-1.00023e-002,8.29610e-003;...
-        0,-6.63788e-003,4.55999e-003;...
-        0,-5.15982e-003,3.75306e-003],... % 315 Hz
-        [0,-1.31230e-002,1.04220e-002;...
-        0,-9.02274e-003,5.73132e-003;...
-        0,-6.94543e-003,4.71734e-003],... % 400 Hz
-        [0,-1.73693e-002,1.30947e-002;...
-        0,-1.24176e-002,7.20526e-003;...
-        0,-9.46002e-003,5.93145e-003],... % 500 Hz
-        [0,-2.31934e-002,1.64308e-002;...
-        0,-1.73009e-002,9.04761e-003;...
-        0,-1.30358e-002,7.44926e-003],... % 630 Hz
-        [0,-3.13292e-002,2.06370e-002;...
-        0,-2.44342e-002,1.13731e-002;...
-        0,-1.82108e-002,9.36778e-003],... % 800 Hz
-        [0,-4.28261e-002,2.59325e-002;...
-        0,-3.49619e-002,1.43046e-002;...
-        0,-2.57855e-002,1.17912e-002],... % 1000 Hz
-        [0,-5.91733e-002,3.25054e-002;...
-        0,-5.06072e-002,1.79513e-002;...
-        0,-3.69401e-002,1.48094e-002],... % 1250 Hz
-        [0,-8.26348e-002,4.05894e-002;...
-        0,-7.40348e-002,2.24476e-002;...
-        0,-5.34977e-002,1.85371e-002],... % 1600 Hz
-        [0,-1.17018e-001,5.08116e-002;...
-        0,-1.09516e-001,2.81387e-002;...
-        0,-7.85097e-002,2.32872e-002],... % 2000 Hz
-        [0,-1.67714e-001,6.37872e-002;...
-        0,-1.63378e-001,3.53729e-002;...
-        0,-1.16419e-001,2.93723e-002],... % 2500 Hz
-        [0,-2.42528e-001,7.98576e-002;...
-        0,-2.45161e-001,4.43370e-002;...
-        0,-1.73972e-001,3.70015e-002],... % 3150 Hz
-        [0,-3.53142e-001,9.96330e-002;...
-        0,-3.69163e-001,5.53535e-002;...
-        0,-2.61399e-001,4.65428e-002],... % 4000 Hz
-        [0,-5.16316e-001,1.24177e-001;...
-        0,-5.55473e-001,6.89403e-002;...
-        0,-3.93998e-001,5.86715e-002],... % 5000 Hz
-        [0,-7.56635e-001,1.55023e-001;...
-        0,-8.34281e-001,8.58123e-002;...
-        0,-5.94547e-001,7.43960e-002],... % 6300 Hz
-        [0,-1.10165e+000,1.91713e-001;...
-        0,-1.23939e+000,1.05243e-001;...
-        0,-8.91666e-001,9.40354e-002],... % 8000 Hz
-        [0,-1.58477e+000,2.39049e-001;...
-        0,-1.80505e+000,1.28794e-001;...
-        0,-1.32500e+000,1.21333e-001],... % 10000 Hz
-        [0,-2.50630e+000,1.42308e-001;...
-        0,-2.19464e+000,2.76470e-001;...
-        0,-1.90231e+000,1.47304e-001]); % 12500 Hz
-    
-    % filter gains
-    filtgain = [4.30764e-011;... % 25 Hz
-        8.59340e-011;... % 31.5 Hz
-        1.71424e-010;... % 40 Hz
-        3.41944e-010;... % 50 Hz
-        6.82035e-010;... % 63 Hz
-        1.36026e-009;... % 80 Hz
-        2.71261e-009;... % 100 Hz
-        5.40870e-009;... % 125 Hz
-        1.07826e-008;... % 160 Hz
-        2.14910e-008;... % 200 Hz
-        4.28228e-008;... % 250 Hz
-        8.54316e-008;... % 315 Hz
-        1.70009e-007;... % 400 Hz
-        3.38215e-007;... % 500 Hz
-        6.71990e-007;... % 630 Hz
-        1.33531e-006;... % 800 Hz
-        2.65172e-006;... % 1000 Hz
-        5.25477e-006;... % 1250 Hz
-        1.03780e-005;... % 1600 Hz
-        2.04870e-005;... % 2000 Hz
-        4.05198e-005;... % 2500 Hz
-        7.97914e-005;... % 3150 Hz
-        1.56511e-004;... % 4000 Hz
-        3.04954e-004;... % 5000 Hz
-        5.99157e-004;... % 6300 Hz
-        1.16544e-003;... % 8000 Hz
-        2.27488e-003;... % 10000 Hz
-        3.91006e-003];   % 12500 Hz
-    
-    filteredaudio = zeros(len,28);
-    
-    for n = 1:28
-        filteredaudio(:,n) = filtgain(n) * filter(br(3,:),ar(3,:)-ad(3,:,n),...
-            filter(br(2,:),ar(2,:)-ad(2,:,n),...
-            filter(br(1,:),ar(1,:)-ad(1,:,n),audio)));
-    end
-    
-    %% **************************************************************
-    % STEP 3 - Squaring and smoothing by 3 1st order lowpass filters
-    % ***************************************************************
-    
-    filteredaudio = filteredaudio.^2;
-    
-    ThirdOctaveLevel = zeros(NumSamplesLevel,28);
-    
-    for i = 1:28
         
-        if method == 2 % time-varying from audio signal
-            
-            smoothedaudio = zeros(len,28);
-            
-            if CenterFrequency(i) <= 1000
-                Tau = 2/(3*CenterFrequency(i));
-            else
-                Tau = 2/(3*1000.);
-            end
-            
-            % 3x smoothing 1st order low-pass filters in series
-            A1 = exp(-1 ./ (fs * Tau));
-            B0 = 1 - A1;
-            Y1 = 0;
-            for k = 1:3
-                for j = 1:length(filteredaudio)
-                    %                 smoothedaudio(j,i) = A1*temp(j,i) + B0*Y1;
-                    smoothedaudio(j,i)= (B0*filteredaudio(j,i))+(A1*Y1); % <----- modified from original by gfg
-                    Y1 = smoothedaudio(j,i);
-                end
-            end
-            
-            c=1;
-            for j = 1:NumSamplesLevel
-                ThirdOctaveLevel(j,i) = 10*log10((smoothedaudio(c,i)+TINY_VALUE)/I_REF);
-                c = c+DecFactorLevel;
-            end
-            
-        elseif method == 1 % stationary from audio signal
-            
-            NumSkip = floor(time_skip * fs);
-            smoothedaudio = zeros(len-NumSkip,28);
-            
-            if NumSkip > len/2
-                warndlg('time signal too short');
-            end
-            
-            if NumSkip == 0; NumSkip = 1;end
-            smoothedaudio(1:len-NumSkip,i) = filteredaudio(NumSkip:len-1,i);
-            %         ThirdOctaveLevel(NumSamplesLevel,i) = 10*log10((sum(smoothedaudio(:,i))/len+TINY_VALUE)/I_REF);
-            ThirdOctaveLevel(NumSamplesLevel,i) = 10*log10((sum(smoothedaudio(:,i)/len)+TINY_VALUE)/I_REF); % <----- modified from original by gfg
-        end
-        
-    end
+        ThirdOctaveLevel=insig; % get 1/3 octave levels from insig if method = 0
 
-else % if method == 0
+    otherwise
+        % if different from stationary (from input 1/3 octave unweighted SPL)
     
-    SampleRateLevel = 1;
-    DecFactorLoudness = 1;
-    NumSamplesLevel = 1;
+        audio=insig; % input signal
+        
+        if fs ~= 48000
+            gcd_fs = gcd(48000,fs); % greatest common denominator
+            audio = resample(audio,48000/gcd_fs,fs/gcd_fs);
+            fs = 48000;
+            len = size(audio,1);
+        else
+            len = size(audio,1);
+        end
+
+        switch method
+            case 1 % stationary from audio signal
+                SampleRateLevel = 1;
+                NumSamplesLevel = 1;
+            
+            case 2 % time_varying from audio signal
+                SampleRateLevel = SR_LEVEL;
+                DecFactorLevel = fs/SampleRateLevel;
+                NumSamplesLevel = ceil(len/DecFactorLevel);
+        end
+
+        % **************************************************
+        % STEP 2 - Create filter bank and filter the signal 
+        % **************************************************
+        [filteredaudio,fc] = Do_OB13(insig,fs,dBFS);
+        
+        % ***************************************************************
+        % STEP 3 - Squaring and smoothing by 3 1st order lowpass filters
+        % ***************************************************************
+        filteredaudio = filteredaudio.^2;
+
+        N_bands = length(fc);
+        ThirdOctaveLevel = zeros(NumSamplesLevel,N_bands);
+        CentreFrequency = fc;
+        for i = 1:N_bands
+            
+            switch method
+                case {2,'time-varying'} % time-varying from audio signal
+                    smoothedaudio = zeros(len,N_bands);
+                    
+                    if CentreFrequency(i) <= 1000
+                        Tau = 2/(3*CentreFrequency(i));
+                    else
+                        Tau = 2/(3*1000.);
+                    end
+                    
+                    % 3x smoothing 1st order low-pass filters in series
+                    A1 = exp(-1 ./ (fs * Tau));
+                    B0 = 1 - A1;
+                    Y1 = 0;
+                    for k = 1:3
+                        for j = 1:length(filteredaudio)
+                            %                 smoothedaudio(j,i) = A1*temp(j,i) + B0*Y1;
+                            smoothedaudio(j,i)= (B0*filteredaudio(j,i))+(A1*Y1); % <----- modified from original by gfg
+                            Y1 = smoothedaudio(j,i);
+                        end
+                    end
+                    
+                    c=1;
+                    for j = 1:NumSamplesLevel
+                        ThirdOctaveLevel(j,i) = 10*log10((smoothedaudio(c,i)+TINY_VALUE)/I_REF);
+                        c = c+DecFactorLevel;
+                    end
+                    
+                case {1,'stationary'} % stationary from audio signal        
+                    NumSkip = floor(time_skip * fs);
+                    smoothedaudio = zeros(len-NumSkip,28);
+                    
+                    if NumSkip > len/2
+                        warndlg('time signal too short');
+                    end
+                    
+                    if NumSkip == 0; NumSkip = 1; end
+                    smoothedaudio(1:len-NumSkip,i) = filteredaudio(NumSkip:len-1,i);
+                    %         ThirdOctaveLevel(NumSamplesLevel,i) = 10*log10((sum(smoothedaudio(:,i))/len+TINY_VALUE)/I_REF);
+                    ThirdOctaveLevel(NumSamplesLevel,i) = 10*log10((sum(smoothedaudio(:,i)/len)+TINY_VALUE)/I_REF); % <----- modified from original by gfg
+        
+            end
+        end
+
+        % *******************************************************************
+        % Assign values to global variables according to the selected method
+        % *******************************************************************
     
-    ThirdOctaveLevel=insig; % get 1/3 octave levels from insig if method = 0
+        switch method
+            case 1 % stationary from audio signal
+                SampleRateLevel = 1;
+                NumSamplesLevel = 1;
+                DecFactorLoudness = 1;
+                
+            case 2 % time_varying from audio signal
+                SampleRateLevel = SR_LEVEL;
+                SampleRateLoudness = SR_LOUDNESS;
+                DecFactorLevel = fs/SampleRateLevel;
+                DecFactorLoudness = SampleRateLevel/SampleRateLoudness;
+                NumSamplesLevel = ceil(len/DecFactorLevel);
+                NumSamplesLoudness = ceil(NumSamplesLevel/DecFactorLoudness);
+        end
+    
+        
 end
 
 %% ***********************************************************
