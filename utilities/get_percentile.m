94d81cd71a8a50f00a1026fc63afad736b8c6387
Gil Felix Greco <67735211+ggrecow@users.noreply.github.com>
2025-02-16 18:39:34 +0100

    Update get_percentile.m
    Check and correct dimension of input vector if necessary before anything.

--- utilities/get_percentile.m
+++ utilities/get_percentile.m
@@ -1,4 +1,4 @@
-function OUT = get_percentile(input, PercentValue)
+unction OUT = get_percentile(input, PercentValue)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % Calculate the value exceeded  during <PercentValue> of the time
@@ -20,24 +20,32 @@ function OUT = get_percentile(input, PercentValue)
 %   P90=get_percentile(y,90); % value exceed 90% of the time
 %   yline(P90);
 %
+% Last checked: Gil Felix Greco, 16.02.2025
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-X_index = floor( (100-PercentValue)/100 * length(input) );
+% check input dimension (only [Nx1], [Nx2] or [Nx3] are valid)
+if  size(input,1) > 3 & size(input,2) > 3 % insig has more than 3 channels
+    error('Error: Input signal has more than 3 channels. ')
+elseif  size(input, 2) > 3  % insig is [1xN], [2xN] or [3xN]
+    input = input';
+    % fprintf('\nWarning: Input signal is not [Nx1], [Nx2], or or [Nx3] and was transposed.\n');
+end
+
+X_index = floor( (100-PercentValue)/100 * size(input,1) );
 
 if X_index==0
     X_index=1;
 else
 end
 
-if min(size(input)) > 1 % Case with more than one audio channels
-    OUT = zeros(1,size(input,2)); % declare output variable
-    for nColumns = 1:min(size(input)) % operate on the columns (time vector must be a column vector)
-        sort_input = sort( input(:,nColumns) );
-        OUT(1,nColumns) = sort_input( X_index );
-    end
-else  % Case with only one audio channel
-    sort_input = sort(input);
-    OUT= sort_input( X_index );
+OUT = zeros(1,size(input,2)); % declare output variable
+
+for nColumns = 1:size(input,2) % operate on the columns (time vector must be a column vector)
+
+    sort_input = sort( input(:,nColumns) );
+
+    OUT(1,nColumns) = sort_input( X_index );
+
 end
 
 end
