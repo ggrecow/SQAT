07daec30dee06a88af1189322b88187393f934f4
Gil Felix Greco <67735211+ggrecow@users.noreply.github.com>
2025-03-10 11:31:47 +0100

    Update sone2phon_local.m

--- utilities/sone2phon_local.m
+++ utilities/sone2phon_local.m
@@ -1,7 +1,7 @@
 function phon = sone2phon_local(sone)
-    % Converts loudness from sone to phon according to ISO 532-1:2017
-    % Input: sone - Loudness in sone 
-    % Output: phon - Loudness level in phon
+% Converts loudness from sone to phon according to ISO 532-1:2017
+% Input: sone - Loudness in sone
+% Output: phon - Loudness level in phon
     
     % check if sone is a column vector, and correct if its not
     if size(sone,2)>1
@@ -15,9 +15,9 @@ function phon = sone2phon_local(sone)
     idx = (sone>=1);
 
     % calculate phon values for sone >= 1
-    phon(idx) = 40 + 10 * log2( sone( idx ) );
+    phon(idx) = 40 + 33.22 .* log10( sone( idx ) );
 
     % calculate phon values for  sone < 1
-    phon(~idx) = 40.*sone(~idx).^0.35;
-
+    phon(~idx) = 40 .* ( sone(~idx) + 0.0005 ).^0.35;
+    
 end
