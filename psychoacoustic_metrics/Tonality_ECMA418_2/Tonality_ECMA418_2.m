a9e635bc273e3d64269813125224a93dd127b2d1
Mike Lotinga <m_lotinga@yahoo.co.uk>
2025-07-23 16:22:28 +0100

    ECMA-418-2:2025 tweaks
    1. Moved all epsilons to standardised value 1e-12 (instead of using eps).
    2. Added explicit indexing for combined binaural specific calculation, to avoid inadvertent miscalculations when using line-by-line inputs rather than function call.
    3. Removed redundant 3rd dimension indexing in 90th percentile roughness calculation.

--- psychoacoustic_metrics/Tonality_ECMA418_2/Tonality_ECMA418_2.m
+++ psychoacoustic_metrics/Tonality_ECMA418_2/Tonality_ECMA418_2.m
@@ -145,7 +145,7 @@ function OUT = Tonality_ECMA418_2(insig, fs, fieldtype, time_skip, show)
 % Institution: University of Salford / ANV Measurement Systems
 %
 % Date created: 07/08/2023
-% Date last modified: 27/06/2025
+% Date last modified: 23/07/2025
 % MATLAB version: 2023b
 %
 % Copyright statement: This file and code is part of work undertaken within
@@ -267,6 +267,9 @@ B = 0.003;
 cal_T = 2.8758615;  % calibration factor in Section 6.2.8 Equation 51 ECMA-418-2:2025 [c_T]
 cal_Tx = 1/0.9999043734252;  % Adjustment to calibration factor (Footnote 22 ECMA-418-2:2025)
 
+% Footnote 14 standardised epsilon
+epsilon = 1e-12;
+
 %% Signal processing
 
 % Input pre-processing
@@ -344,7 +347,7 @@ for chan = size(pn_om, 2):-1:1
                            2*blockSizeDupe(zBand), 1);
         % Section 6.2.2 Equation 29 ECMA-418-2:2025 [phi_l,z(m)]
         denom = sqrt(cumsum(pn_rlz.^2, 1, 'reverse').*flipud(cumsum(pn_rlz.^2)))...
-                + 1e-12;
+                + epsilon;
 
         % note that the block length is used here, rather than the 2*s_b,
         % for compatability with the remaining code - beyond 0.75*s_b is
@@ -434,7 +437,7 @@ for chan = size(pn_om, 2):-1:1
         % Equation 42 ECMA-418-2:2025 signal-noise-ratio first approximation
         % (ratio of tonal component loudness to non-tonal component loudness in critical band)
         % [SNRhat(l,z)]
-        SNRlz1 = bandTonalLoudness./((bandLoudness - bandTonalLoudness) + 1e-12);
+        SNRlz1 = bandTonalLoudness./((bandLoudness - bandTonalLoudness) + epsilon);
 
         % Equation 43 ECMA-418-2:2025 low pass filtered specific loudness
         % of non-tonal component in critical band [Ntilde'_tonal(l,z)]
@@ -480,7 +483,7 @@ for chan = size(pn_om, 2):-1:1
     % Calculation of specific tonality
     % --------------------------------
     % Section 6.2.8 Equation 49 ECMA-418-2:2025 [SNR(l)]
-    overallSNR = max(specTonalLoudness, [], 2)./(1e-12 + sum(specNoiseLoudness, 2));  % loudness signal-noise-ratio
+    overallSNR = max(specTonalLoudness, [], 2)./(sum(specNoiseLoudness, 2) + epsilon);  % loudness signal-noise-ratio
     
     % Section 6.2.8 Equation 50 ECMA-418-2:2025 [q(l)]
     crit = exp(-A*(overallSNR - B));
@@ -505,9 +508,9 @@ for chan = size(pn_om, 2):-1:1
 
         % Section 6.2.9 Equation 53 ECMA-418-2:2025  
         specTonalityAvg(1, zBand, chan)...
-            = sum(specTonality(mask, zBand, chan), 1)./(nnz(mask) + 1e-12); %<--- time index takes <time_skip> into consideration
+            = sum(specTonality(mask, zBand, chan), 1)./(nnz(mask) + epsilon); %<--- time index takes <time_skip> into consideration
         specTonalityAvgFreqs(1, zBand, chan)...
-            = sum(specTonalityFreqs(mask, zBand, chan), 1)./(nnz(mask) + 1e-12); %<--- time index takes <time_skip> into consideration
+            = sum(specTonalityFreqs(mask, zBand, chan), 1)./(nnz(mask) + epsilon); %<--- time index takes <time_skip> into consideration
     end
 
     % Calculation of overall tonality Section 6.2.10
@@ -530,7 +533,7 @@ for chan = size(pn_om, 2):-1:1
 
     % Section 6.2.11 Equation 63 ECMA-418-2:2025
     % Time-averaged total tonality [T]
-    tonalityAvg(chan) = sum(tonalityTDep(mask, chan))/(nnz(mask) + eps); %<--- time index takes <time_skip> into consideration
+    tonalityAvg(chan) = sum(tonalityTDep(mask, chan))/(nnz(mask) + epsilon); %<--- time index takes <time_skip> into consideration
 
 %% Output plotting
 
