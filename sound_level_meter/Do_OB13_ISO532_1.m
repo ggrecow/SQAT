2c859f0e90f5e9aaa1b638bb662c6cd9d5b888c4
Gil Felix Greco <ggrecow@gmail.com>
2026-09-15 16:58:03 +0200

    BSD-3-clause license
    codes originally descendingfrom the loudness ISO 532-1 implementation from AARAE need to be licensed under BSD 3 clause scheme

--- sound_level_meter/Do_OB13_ISO532_1.m
+++ sound_level_meter/Do_OB13_ISO532_1.m
@@ -64,9 +64,24 @@ function [outsig, fc] = Do_OB13_ISO532_1(insig, fs, fmin, fmax)
 %   lvl_from_outsig = 10*log10(sum(10.^(OB_lvls/10))); % '/10' means squared, therefore it is 10*log10()
 %   fprintf('Level obtained from the filtered signals=%.1f dB SPL\n',lvl_from_outsig);
 %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Log
+% 
 % Author: Ella Manor - MATLAB implementation for AARAE (2015)
+% 
 % Author (modifications): Gil Felix Greco (22/02/2023)
+% 
 % Author (stand-alone function): Alejandro Osses (20/10/2023)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright statement: This file and code is subject to the BSD-3 license in
+% its entirety, as detailed in the license text reproduced at the end of
+% this file.
+%
+% As per the licensing information, please be aware that this code is
+% WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if nargin < 3
@@ -256,3 +271,33 @@ fc = CenterFrequency;
 if bLimit_range
     fc = CenterFrequency(idx_fmin:idx_fmax);
 end
+
+%**************************************************************************
+% Copyright (c) <2015>, <Ella Manor>
+% All rights reserved.
+%
+% Redistribution and use in source and binary forms, with or without
+% modification, are permitted provided that the following conditions are
+% met:
+%
+%  * Redistributions of source code must retain the above copyright notice,
+%    this list of conditions and the following disclaimer.
+%  * Redistributions in binary form must reproduce the above copyright
+%    notice, this list of conditions and the following disclaimer in the
+%    documentation and/or other materials provided with the distribution.
+%  * Neither the name of the <ORGANISATION> nor the names of its contributors
+%    may be used to endorse or promote products derived from this software
+%    without specific prior written permission.
+%
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+% TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+% PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
+% OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+% EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+% PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+% PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+% LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+% NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+% SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+%**************************************************************************
