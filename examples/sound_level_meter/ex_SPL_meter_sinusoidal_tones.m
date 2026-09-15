09b540916e53ce200669e15e5674ce2c89cca5e5
Gil Felix Greco <ggrecow@gmail.com>
2026-09-15 17:45:31 +0200

    adopting GPL-3.0 license: examples folder

--- examples/sound_level_meter/ex_SPL_meter_sinusoidal_tones.m
+++ examples/sound_level_meter/ex_SPL_meter_sinusoidal_tones.m
@@ -18,7 +18,18 @@
 %  necessary to run this code.
 %
 %   Gil Felix Greco, 17.11.2024
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright statement: This file is part of the SQAT toolbox and is subject
+% to the GPL-3.0 license, as detailed in <licenses/gpl-3.0.txt> in the SQAT
+% repository root. Some files in SQAT carry a different license, always
+% stated in their own header; where this file depends on them, the combined
+% work remains governed by the GPL-3.0.
+%
+% As per the licensing information, this file is provided "as is", WITHOUT
+% WARRANTY OF ANY KIND, express or implied, including but not limited to the
+% warranties of MERCHANTABILITY and FITNESS FOR A PARTICULAR PURPOSE.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc; clear all; close all;
  
 %% Create tones
