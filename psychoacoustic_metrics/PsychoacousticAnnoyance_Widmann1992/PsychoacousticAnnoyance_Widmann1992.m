6575160e50f01cc9d97ad5dc952ec89a80e39411
Gil Felix Greco <ggrecow@gmail.com>
2026-09-15 17:18:58 +0200

    adopting GPL-3.0 license: PsychoacousticAnnoyance_Widmann1992

--- psychoacoustic_metrics/PsychoacousticAnnoyance_Widmann1992/PsychoacousticAnnoyance_Widmann1992.m
+++ psychoacoustic_metrics/PsychoacousticAnnoyance_Widmann1992/PsychoacousticAnnoyance_Widmann1992.m
@@ -114,6 +114,17 @@ function OUT = PsychoacousticAnnoyance_Widmann1992(insig,fs,LoudnessField,time_s
 % PsychoacousticAnnoyance_Zwicker1999.m
 % Modified: Mike Lotinga, 29.05.2026 - fixed bug caused by axes swap in
 % sharpness following vectorisation
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright statement: This file is part of the SQAT toolbox and is subject
+% to the GPL-3.0 license, as detailed in <licenses/gpl-3.0.txt> in the SQAT
+% repository root. Some files in SQAT carry a different license, always
+% stated in their own header; where this file depends on them, the combined
+% work remains governed by the GPL-3.0.
+%
+% As per the licensing information, this file is provided "as is", WITHOUT
+% WARRANTY OF ANY KIND, express or implied, including but not limited to the
+% warranties of MERCHANTABILITY and FITNESS FOR A PARTICULAR PURPOSE.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin == 0
     help PsychoacousticAnnoyance_Widmann1992;
--- psychoacoustic_metrics/PsychoacousticAnnoyance_Widmann1992/PsychoacousticAnnoyance_Widmann1992_from_wavfile.m
+++ psychoacoustic_metrics/PsychoacousticAnnoyance_Widmann1992/PsychoacousticAnnoyance_Widmann1992_from_wavfile.m
@@ -107,6 +107,17 @@ function OUT = PsychoacousticAnnoyance_Widmann1992_from_wavfile(wavfilename,dBFS
 % Author: Alejandro Osses
 % Modified: Mike Lotinga, 12.06.2025 - created from
 % PsychoacousticAnnoyance_Zwicker1999_from_wavfile.m
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright statement: This file is part of the SQAT toolbox and is subject
+% to the GPL-3.0 license, as detailed in <licenses/gpl-3.0.txt> in the SQAT
+% repository root. Some files in SQAT carry a different license, always
+% stated in their own header; where this file depends on them, the combined
+% work remains governed by the GPL-3.0.
+%
+% As per the licensing information, this file is provided "as is", WITHOUT
+% WARRANTY OF ANY KIND, express or implied, including but not limited to the
+% warranties of MERCHANTABILITY and FITNESS FOR A PARTICULAR PURPOSE.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if nargin == 0
     help PsychoacousticAnnoyance_Widmann1992_from_wavfile;
